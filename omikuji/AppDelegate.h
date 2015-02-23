381ba8257bc2eef7e84544aed85572c53771d503
Nakazawa Namiko <nakazawa@Macintosh.local>
2015-02-23 23:06:08 +0900

    Initial Commit

---
+++ omikuji/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  omikuji
+//
+//  Created by Nakazawa Namiko on 2015/02/23.
+//  Copyright (c) 2015年 Nakazawa Yuri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ omikuji/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  omikuji
+//
+//  Created by Nakazawa Namiko on 2015/02/23.
+//  Copyright (c) 2015年 Nakazawa Yuri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
